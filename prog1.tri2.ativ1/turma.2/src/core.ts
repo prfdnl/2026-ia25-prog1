4a9e150849c70101788170965dabe10f7baea6d8
niel <niel@ganon>
2026-05-14 11:35:19 -0300

    crioado para turma 1 todo list que salva no disco [erro ao buscar lista, precisa esperar ler do disoco]

--- prog1.tri2.ativ1/turma.2/src/core.ts
+++ prog1.tri2.ativ1/turma.2/src/core.ts
@@ -1,39 +1,65 @@
-// class Item { 
+/**
+ * @todo
+ * known issues:
+ * - getItems needs to await loadListFromDisk()
+ */
+
+// class Item_ { 
 //   public title: string
 //   constructor(title: string) {
-//     this.title = title;
+//     this.title = title
 //   }
 // }
 
 class Item {
-  constructor(public item: string) { }
+  constructor(public title: string) { }
 }
 
 class TodoList {
-  private itens: Item[] = [];
-  private filePath: string;
+  private items: Item[] = []
+  private filePath: string
 
   constructor(filePath: string) {
-    this.filePath = filePath;
+    this.filePath = filePath
+    this.readListFromDisk()
   }
 
-  addItem(item: Item) {
-    this.itens.push(item)
+  private async saveListToDisk() {
+    const file = Bun.file(this.filePath)
+    const data = JSON.stringify(this.items)
+    await file.write(data)
   }
 
-  removeItem(index: number) {
-    this.itens.splice(index, 1);
+  private async readListFromDisk() {
+    const file = Bun.file(this.filePath)
+    // const text = await file.text()
+    // const data = JSON.parse(text)
+    const data = await file.json()
+    this.items = data.map((v: any) => {
+      return new Item(v.title)
+    })
   }
 
+  /**
+   * Adiciona um novo item na lista de item
+   */
+  async addItem(item: Item) {
+    this.items.push(item)
+    await this.saveListToDisk()
+  }
+
+  /**
+   * Remove um item da lista de item pelo indice
+   */
+  async removeItem(index: number) {
+    this.items.splice(index, 1)
+    await this.saveListToDisk()
+  }
+
+  /**
+   * Retona uma cópia da lista de itens
+   */
   getItems() {
-    return this.itens
+    return Array.from(this.items)
   }
 }
-
-
-const lista = new TodoList('arquivo.txt')
-lista.addItem(new Item("comprar abacate"))
-lista.addItem(new Item("aprender javascript"))
-lista.removeItem(1)
-lista.addItem(new Item("aprender typescript"))
-console.log(lista.getItems())
